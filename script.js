4e7bb36bb5d905470b1baf76af132d830dd02b6a
Jake Kerr <jakekerr25@gmail.com>
2018-08-19 17:57:55 -0600

    added some loops

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-console.log("hello world");
-console.log("Wes is a teacher")
+// setting and swapping
+var myName = "Jake";
+var myNumber = 42;
+var temp = myName;
+myName = myNumber; 
+myNumber = temp;
+// console.log (myName);
+// console.log (myNumber); 
+
+// print 0 to 255 using a for loop
+// for(var i=0; i<256; i++) {
+//     console.log (i);
+// };
+// var x=0;
+// while(x<256) {
+//     console.log(x);
+//     x++; 
+// }
+// var i=6
+//     while(i<=60000){
+//         console.log(i);
+//         i+=6;
+//     }
