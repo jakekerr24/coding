86075c50ca54d0705c19f00bc64ebc500b579020
Jake Kerr <jakekerr25@gmail.com>
2018-08-19 19:17:59 -0600

    added returns

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // setting and swapping
-// var myName = "Jake";
-// var myNumber = 42;
+var myName = "Jake";
+var myNumber = 42;
 // var temp = myName;
 // myName = myNumber; 
 // myNumber = temp;
@@ -21,14 +21,41 @@
 //         console.log(i);
 //         i+=6;
 //     }
-for(var i=1; i<=100; i++){ 
-    if(i%5===0) {
-        console.log("coding"); 
-    } else {
-        console.log(i);
-    }
 
-    if(i%10==0) {
-        console.log("dojo");
+// for(var i=1; i<=100; i++){ 
+//     if(i%5===0) {
+//         console.log("coding"); 
+//     } else {
+//         console.log(i);
+//     }
+
+//     if(i%10==0) {
+//         console.log("dojo");
+//     }
+// } 
+
+// function whatDoYouKnow(param) {
+//     console.log(param);
+// }
+// whatDoYouKnow("Hello");
+
+// function flexCountdown(lowNum, hiNum, mult) {
+//     for(var i=hiNum; i>=lowNum; i--){
+//         if(i%mult==0){
+//             console.log(i);
+//         }    
+//     }
+// }
+// flexCountdown(2,51,3);
+
+function countDown(num) {
+    var arr=[];
+    
+    for(i=num; i>=0; i--){
+        arr.push(i);
     }
-} 
+    // console.log(arr);
+    return arr;
+}
+var x=countDown(14);
+console.log(x);
